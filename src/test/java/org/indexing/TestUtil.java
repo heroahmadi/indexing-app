374a4f9e0129c8fa0e0da09377e28ceadf92fec6
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2024-07-31 06:06:45 +0700

    generate and test more files

--- src/test/java/org/indexing/TestUtil.java
+++ src/test/java/org/indexing/TestUtil.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 
 public class TestUtil {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        generateBigFiles();
+    }
+
+    private static void generateBigFiles() {
         String sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n" +
                 "1234567890 !@#$%^&*()_+-=[]{}|;:',.<>/?`~\n\n" +
                 "Sed ut perspiciatis unde omnis iste natus   erro r sit voluptatem accusantium Doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.\n" +
@@ -15,19 +19,23 @@ public class TestUtil {
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt molliD Z anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.\n" +
                 "1234567890 !@#$%^&*()_+-=[]{}|;:',.<>/?`~\n";
 
-        String fileName = "test_file2.txt";
-        int targetSizeMB = 2;
+        String fileName = "test_file";
+        int numFile = 15;
+        int targetSizeMB = 15;
         long targetSizeBytes = targetSizeMB * 1024 * 1024;
 
-        try (FileWriter writer = new FileWriter(fileName)) {
-            long bytesWritten = 0;
-            while (bytesWritten < targetSizeBytes) {
-                writer.write(sampleText);
-                bytesWritten += sampleText.getBytes().length;
+        for (int i=1; i<=numFile; i++) {
+            String completeFileName = fileName + i + ".txt";
+            try (FileWriter writer = new FileWriter(completeFileName)) {
+                long bytesWritten = 0;
+                while (bytesWritten < targetSizeBytes) {
+                    writer.write(sampleText);
+                    bytesWritten += sampleText.getBytes().length;
+                }
+                System.out.println("File created successfully: " + completeFileName);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            System.out.println("File created successfully: " + fileName);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
--- src/test/java/org/indexing/service/IndexingServiceTest.java
+++ src/test/java/org/indexing/service/IndexingServiceTest.java
@@ -2,6 +2,10 @@ package org.indexing.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -13,10 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class IndexingServiceTest {
 
     @Test
-    public void testWithSmallFile() throws ExecutionException, InterruptedException {
+    public void testWithSmallFile() throws ExecutionException, InterruptedException, IOException {
         List<String> fileNames = asList("input.txt");
         IndexingService service = new IndexingService();
-        List<String> expected = asList("<qe23oasfasjk", "Asa", "qeqioroi2u12321", "Aq");
+        List<String> expected = asList("Test", "<qe23oasfasjk", "Asa", "qeqioroi2u12321", "Aq");
         List<String> output = service.run(fileNames);
         assertNotNull(output);
         expected.sort(Comparator.naturalOrder());
@@ -26,11 +30,23 @@ public class IndexingServiceTest {
     }
 
     @Test
-    public void testWithLargeFile() throws ExecutionException, InterruptedException {
-        List<String> fileNames = asList("test_file1.txt", "test_file2.txt");
+    public void testWithLargeFile() throws ExecutionException, InterruptedException, IOException {
+        int numFiles = 15;
+        List<String> fileNames = new ArrayList<>();
+        for (int i=1; i<=numFiles; i++) {
+            fileNames.add("test_file" + i + ".txt");
+        }
         IndexingService service = new IndexingService();
+        long start = System.currentTimeMillis();
         List<String> output = service.run(fileNames);
+        long end = System.currentTimeMillis();
+        System.out.println("Finished. Duration: " + (end - start) + " millis");
         assertNotNull(output);
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        for (String str : output) {
+            writer.write(str + System.lineSeparator());
+        }
     }
 
 }
