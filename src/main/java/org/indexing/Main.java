9834d7029f6eb9d7843eed8d8cc242f91b734914
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2024-07-31 06:06:33 +0700

    output to file

--- src/main/java/org/indexing/Main.java
+++ src/main/java/org/indexing/Main.java
@@ -2,6 +2,10 @@ package org.indexing;
 
 import org.indexing.service.IndexingService;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static java.util.Arrays.asList;
@@ -10,7 +14,15 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         IndexingService service = new IndexingService();
-        service.run(asList(args));
+        List<String> output = service.run(asList(args));
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            for (String str : output) {
+                writer.write(str + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
