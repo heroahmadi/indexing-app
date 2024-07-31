a7478af48b63c2a1e0e7d0cbb2481ce299f2a4d9
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2024-07-31 10:07:41 +0700

    add runtime after program finished

--- src/main/java/org/indexing/Main.java
+++ src/main/java/org/indexing/Main.java
@@ -13,8 +13,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
         try {
             IndexingService service = new IndexingService();
+            long startMillis = System.currentTimeMillis();
             service.run(asList(args));
-            System.out.println("Finished successfully. Check the output file in the output_***.txt");
+            long endMillis = System.currentTimeMillis();
+            float runtimeSeconds = (float) (endMillis - startMillis) / 1000;
+            System.out.printf("Runtime in seconds: %f\nFinished successfully. Check the output file in the output_***.txt\n",runtimeSeconds);
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
